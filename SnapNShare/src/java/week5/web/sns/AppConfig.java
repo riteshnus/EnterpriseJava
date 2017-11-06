17ba4ac82ae4ef2fe79f22da00774ad9896b3349
Ritesh <ritesh.nus@gmail.com>
2017-11-06 10:30:15 +0800

    Intial Setup

---
+++ SnapNShare/src/java/week5/web/sns/AppConfig.java
@@ -0,0 +1,13 @@
+package week5.web.sns;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Ritesh
+ */
+@ApplicationPath("api")
+public class AppConfig extends Application{
+    
+}
---
+++ SnapNShare/src/java/week5/web/sns/Friends.java
@@ -0,0 +1,31 @@
+package week5.web.sns;
+
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Ritesh
+ */
+@RequestScoped
+@Path("/friends")
+public class Friends {
+    
+    
+        @GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public void get() {
+//
+//		
+//		return Response.ok();
+	}
+}
